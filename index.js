e111b4715b31f58a07e9d9cb411af488630cbc7c
MMonirHossain <monirimu2013@gmail.com>
2023-08-03 02:39:26 +0600

    Added background process and refactor the code

--- index.js
+++ index.js
@@ -5,70 +5,25 @@
 */
 
 //dependencies 
-const http = require('http');
+const server = require('./lib/server.js');
+const workers = require('./lib/worker.js');
 
-const {handleReqRes}= require('./controller/handleReqRes.js');
-const environment= require('./controller/environment.js');
-//const dataa = require('./lib/data.js');
-const {sentTwilioSms}= require('./controller/notifications.js');
 
 //app object - module scaffolding
 
 const app = {};
 
-//testing the notification
-sentTwilioSms('01948325474','Hello World',(err)=>{
-    console.log(err);
-});
+app.init = ()=>{
+    //start the server
+    server.init();
 
-
-/*
-* testing file system functions
-
-* test create file funciton
-dataa.create('test','newFile',{'name':'Monir'},(err)=>{
-    console.log(err);
-});
-
-*test read file function
-dataa.read('test','newFile', (err,data)=>{
-    if(!err){console.log(data);}
-    else console.log(err);
-});
-
-*test update file funciton
-dataa.update('test','newFile',{'name':'Tarek'},(err)=>{
-    console.log(err);
-});
-
-
-*test delete file function
-dataa.delete('test','newFile', (err)=>{
-    console.log(err);
-});
-
-*/
-
-// configuration
-
-app.config ={
-    port: environment.port,
-    envName: environment.envName,
+    // start the worker
+    workers.init();
 };
 
-//create server
-app.createServer= ()=>{
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, ()=>{
-        
-        console.log(`listening to port ${app.config.port}`);
-        console.log(`Environment Variable: ${app.config.envName}`);
-    });
-};
+app.init();
 
 
-// handle request and responce
-app.handleReqRes = handleReqRes;
+//exporting the app
 
-//start the server
-app.createServer();
+module.exports = app;
